85b30dee31a393757742a4782384952af54c12ab
Ilias Bergström <ilias@elk.audio>
2024-02-14 11:29:59 +0100

    Added suppressor

---
+++ Examples/example.cpp
@@ -0,0 +1,14 @@
+#include <elk-warning-suppressor/warning_suppressor.hpp>
+
+int main()
+{
+    unsigned int one = 0;
+    int two = 0;
+
+    ELK_PUSH_WARNING
+    ELK_DISABLE_SIGN_CONVERSION
+    two = one; // This warning will be suppressed when building.
+    ELK_POP_WARNING
+
+    return 0;
+}
